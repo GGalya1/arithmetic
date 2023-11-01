d928aa52d499b374438f357c7084b750a6647593
GGalya <garturmail@gmail.com>
2023-11-01 10:08:12 +0100

    structure:

---
+++ src/main/scala/ArithmeticExpression.scala
@@ -0,0 +1,11 @@
+object ArithmeticExpression {
+  git
+  enum ArithmeticExpression:
+    case Num(x: Int)
+    case Minus(x: ArithmeticExpression)
+    case Plus(x: ArithmeticExpression, y: ArithmeticExpression)
+    case Mult(x: ArithmeticExpression, y: ArithmeticExpression)
+    case Div(x: ArithmeticExpression, y: ArithmeticExpression)
+
+
+}
